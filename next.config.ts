0913b4b31d3566e8de2d761e31b12bcf7e9929c7
Win-20251017RIQ\Administrator <hebei5920>
2025-12-19 16:39:09 +0800

    1\

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,10 @@
 import type { NextConfig } from "next";
 
+// 初始化 OpenNext Cloudflare 开发环境
+// 这使得在本地开发时可以模拟 Cloudflare Workers 环境
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
+initOpenNextCloudflareForDev();
+
 const nextConfig: NextConfig = {
   /* config options here */
 };
